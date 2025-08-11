a2c501d50d87fb5da6af8725ea4ad9102ef91f1a
dev02-hue <trustwallet2758604@gmail.com>
2025-08-11 09:06:07 +0100

    chat

--- src/app/component/user/SmartSuppScript.tsx
+++ src/app/component/user/SmartSuppScript.tsx
@@ -3,30 +3,28 @@
 import Script from "next/script"
 import { usePathname } from "next/navigation"
 
-export default function SmartSuppScript() {
+export default function TawkToScript() {
   const pathname = usePathname()
 
-  const isUserOrAdminPage = pathname.startsWith("/user") || pathname.startsWith("/savio")
+  const isUserOrAdminPage =
+    pathname.startsWith("/user") || pathname.startsWith("/savio")
 
   if (isUserOrAdminPage) return null
 
   return (
-    <>
-      <Script id="smartsupp-script" strategy="afterInteractive">
-        {`
-          var _smartsupp = _smartsupp || {};
-          _smartsupp.key = 'be0ade35033b31a67574449182c53f971931da5b';
-          window.smartsupp||(function(d) {
-            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-            c.type='text/javascript';c.charset='utf-8';c.async=true;
-            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-          })(document);
-        `}
-      </Script>
-      <noscript>
-        Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a>
-      </noscript>
-    </>
+    <Script id="tawkto-script" strategy="afterInteractive">
+      {`
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+          var s1=document.createElement("script"),
+              s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/6899a0ddd866aa19294cd231/1j2c1oovl';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+        })();
+      `}
+    </Script>
   )
 }
